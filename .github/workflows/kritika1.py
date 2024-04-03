0655cf7d544cf44a0fc587abe4040fcc311df85f
LAKSHAY MAHINDRO <107855307+LAKSHAYMAHINDRO@users.noreply.github.com>
2024-04-03 11:49:45 +0530

    Update kritika1.py

--- .github/workflows/kritika1.py
+++ .github/workflows/kritika1.py
@@ -3,6 +3,6 @@ import random
 def generate_random_number(start, end):
     return random.randint(start, end)
 
-# Example usage:
+# Generate a random number each time the script is run
 random_number = generate_random_number(1, 100)
 print("Random number:", random_number)
