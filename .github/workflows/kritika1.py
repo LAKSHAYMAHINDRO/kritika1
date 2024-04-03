e7d820dd8b117a25728981f669861c2ba0b59d46
LAKSHAY MAHINDRO <107855307+LAKSHAYMAHINDRO@users.noreply.github.com>
2024-04-03 11:51:31 +0530

    Update kritika1.py

--- .github/workflows/kritika1.py
+++ .github/workflows/kritika1.py
@@ -1,8 +1,9 @@
 import random
 
-def generate_random_number(start, end):
-    return random.randint(start, end)
+def main():
+    # Generate a random number each time the script is run
+    random_number = random.randint(1, 100)
+    print("Random number:", random_number)
 
-# Generate a random number each time the script is run
-random_number = generate_random_number(1, 100)
-print("Random number:", random_number)
+if __name__ == "__main__":
+    main()
